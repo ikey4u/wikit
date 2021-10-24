9dbf5ea66df269a5be1495e7876b76b63ae4399d
ikey4u <pwnkeeper@gmail.com>
2021-10-24 22:58:22 +0800

    feat: add desktop dev template

---
+++ desktop/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<main>
+  <div class="header">
+    MENU
+  </div>
+  <div class="page">
+    <div class="page-content">
+      HTML
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+  }
+
+  .header {
+  }
+
+  .page {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  .page-content {
+    flex-grow: 1;
+    overflow: auto;
+    min-height: 2em;
+  }
+</style>
