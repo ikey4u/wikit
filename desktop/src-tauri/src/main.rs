96aa215b06097689da561525b2a5870f11f9c4d9
ikey4u <pwnkeeper@gmail.com>
2021-10-26 01:04:12 +0800

    feat: add basic native menu and search ui

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -3,8 +3,45 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::api::dialog;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let file_submenu = Submenu::new("File", Menu::new().add_item(open).add_item(close));
+
+    let about = CustomMenuItem::new("about".to_string(), "About");
+    let about_submenu = Submenu::new("Help", Menu::new().add_item(about));
+
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(file_submenu)
+        .add_submenu(about_submenu);
+    tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+                "close" => {
+                    println!("click close menu");
+                    std::process::exit(0);
+                },
+                "open" => {
+                    dialog::FileDialogBuilder::default().pick_file(|path| {
+                        if let Some(path) = path {
+                            println!("open file {}", path.display());
+                        } else {
+                            println!("open nothing");
+                        }
+                    });
+                },
+                "about" => {
+                    println!("click about menu");
+                },
+                _ => {}
+           }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- desktop/src/App.svelte
+++ desktop/src/App.svelte
@@ -3,7 +3,11 @@
 
 <main>
   <div class="header">
-    MENU
+    <div class="button options-button">+</div>
+    <div class="search">
+      <input class="search-input" type="text" />
+      <div class="button search-button">GO</div>
+    </div>
   </div>
   <div class="page">
     <div class="page-content">
@@ -23,17 +27,67 @@
   }
 
   .header {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .button {
+    user-select: none;
+    cursor: pointer;
+  }
+
+  .options-button {
+    margin-left: 20px;
+    margin-right: 40px;
+    font-size: 30px;
+    height: 40px;
+    width: 40px;
+    text-align: center;
+    background: blue;
+    color: white;
+    border-radius: 50%;
+  }
+
+  .search {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+  }
+
+  .search-input {
+    flex: 1;
+    margin: auto;
+  }
+
+  .search-button {
+    margin-left: 10px;
+    vertical-align: middle;
+    text-align: center;
+    color: #444;
+    border: 1px solid #CCC;
+    background: #DDD;
+    max-width: 100px;
+    padding: 5px;
   }
 
   .page {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
+    padding: 20px 30px 10px 30px;
   }
 
   .page-content {
     flex-grow: 1;
     overflow: auto;
     min-height: 2em;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 </style>
